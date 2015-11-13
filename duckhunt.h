28740187895db2a733d48ee6472d22471bfe7365
Roman Rieunier <rieunierroman@hotmail.fr>
2015-11-13 10:32:10 +0100

    Update duckhunt.h

--- duckhunt.h
+++ duckhunt.h
@@ -9,17 +9,32 @@
 #include <SDL/SDL_ttf.h>
 #include <sstream>
 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 800;
+const int SCREEN_BPP=32;
+const int NB_CANARD_TO_WIN = 6;
+const int NB_CANARD_PAR_NIVEAU = 10;
+
 struct Canard
 {
-  int couleur;   //0 noir 1 rouge et 2 bleu
-  int vitesse;    //vitesse du canard (pour gérer son déplacement)
-  int posX;       //position x sur le champ de tir
-  int posY;       //position y sur le champ de tir
-  ... et encore plusieurs autres....
+  int c;       //0 noir 1 rouge et 2 bleu
+  int v;       //vitesse du canard (pour gérer son déplacement)
+  int x;       //position x sur le champ de tir
+  int y;       //position y sur le champ de tir
+  int w;       // largeur du canard
+  int h;       // longueur du canard
+  int mvt;     // mouvement du canard
+  //... et encore plusieurs autres....
+};
+
+struct Souris
+{
+    int x;
+    int y;
 };
 
 SDL_Surface *load_image(std::string filename);
-SDL_Surface * loadImageWithColorKey(string filename, int r, int g, int b);
+SDL_Surface * loadImageWithColorKey(std::string filename, int r, int g, int b);
 void applySurface(int x, int y, SDL_Surface* source,
                                 SDL_Surface* destination, SDL_Rect* clip);
 bool collision(SDL_Surface* Screen, Canard D);
