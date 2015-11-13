16edf5110636f42bb4c619239ead2d0c2fb45658
Roman Rieunier <rieunierroman@hotmail.fr>
2015-11-13 10:33:17 +0100

    Update fonctions.cc

--- fonctions.cc
+++ fonctions.cc
@@ -60,112 +60,121 @@ void applySurface(int x, int y, SDL_Surface* source,
     offset.x = x;
     offset.y = y;
     SDL_BlitSurface( source, clip, destination, &offset );
-    void showMessageScreen(string message,int x,int y, TTF_Font *font,int fontSize,SDL_Color textColor,SDL_Surface* &screen)
-    {
-        string mot="";
-        string space=" ";
-        int i=0,j;
-        SDL_Surface *mes=NULL;
+}
 
-        j = message.find(space);
-        while( j != string::npos )
+void showMessageScreen(string message,int x,int y, TTF_Font *font,int fontSize,SDL_Color textColor,SDL_Surface* &screen)
+{
+    string mot="";
+    string space=" ";
+    int i=0,j;
+    SDL_Surface *mes=NULL;
+
+    j = message.find(space);
+    while( j != string::npos )
+    {
+        mot=message.substr(i,j-i);
+        if(mot != "")
         {
-            mot=message.substr(i,j-i);
-            if(mot != "")
-            {
-                mes=TTF_RenderText_Solid(font,mot.c_str(),textColor);
-                applySurface(x,y,mes,screen,NULL);
-                x+=mes->w;
-                SDL_FreeSurface(mes);
-            }
-            x+=fontSize;
-            i=j+1;
-            j = message.find(space,i);
+            mes=TTF_RenderText_Solid(font,mot.c_str(),textColor);
+            applySurface(x,y,mes,screen,NULL);
+            x+=mes->w;
+            SDL_FreeSurface(mes);
         }
-
-        mot=message.substr(i);
-        mes=TTF_RenderText_Solid(font,mot.c_str(),textColor);
-        applySurface(x,y,mes,screen,NULL);
-        SDL_FreeSurface(mes);
+        x+=fontSize;
+        i=j+1;
+        j = message.find(space,i);
     }
 
-
-    struct joueur
-    {
-        int x;
-        int y;
-        int w;
-        int h;
-        SDL_Surface *source;
-        SDL_Rect lecture;
-        SDL_Rect lectureFront;
-        SDL_Rect lectureBack;
-
-    };
-
-    struct ball
-    {
-        int x;
-        int y;
-        int w;
-        int h;
-        SDL_Surface *source;
-        SDL_Rect lecture;
-        SDL_Rect lectureFront;
-    };
-
-    void initjoueur(joueur &s)
-    {
-        s.x=10;
-        s.y=SCREEN_HEIGHT/2;
-        s.w=15;
-        s.h=59;
-        s.lectureFront.x=0;
-        s.lectureFront.y=0;
-        s.lectureFront.w=15;
-        s.lectureFront.h=59;
-        s.lecture = s.lectureFront;
-    }
-
-    void initball(ball &b)
-    {
-        b.x=SCREEN_WIDTH/2;
-        b.y=SCREEN_HEIGHT/2;
-        b.h=16;
-        b.w=16;
-        b.lectureFront.x=0;
-        b.lectureFront.y=0;
-        b.lectureFront.w=16;
-        b.lectureFront.h=16;
-        b.lecture = b.lectureFront;
-    }
+    mot=message.substr(i);
+    mes=TTF_RenderText_Solid(font,mot.c_str(),textColor);
+    applySurface(x,y,mes,screen,NULL);
+    SDL_FreeSurface(mes);
+}
 
 
-    bool collision(SDL_Surface* Screen, Canard D)
-    {
-        int leftD;
-        int rightD;
-        int topD;
-        int bottomD;
+struct joueur
+{
+    int x;
+    int y;
+    int w;
+    int h;
+    SDL_Surface *source;
+    SDL_Rect lecture;
+    SDL_Rect lectureFront;
+    SDL_Rect lectureBack;
 
-        leftS = s.x;
-        rightS = s.x + s.w;
-        topS = s.y;
-        bottomS = s.y + s.h;
+};
 
-        leftD = d.x;
-        rightD = d.x + d.w;
-        topD = d.y;
-        bottomD = d.y + d.h;
+struct ball
+{
+    int x;
+    int y;
+    int w;
+    int h;
+    SDL_Surface *source;
+    SDL_Rect lecture;
+    SDL_Rect lectureFront;
+};
 
-        if(bottomS >= topD)
-            return false;
-        if(topS <= bottomD)
-            return false;
-        if(rightS >= leftD)
-            return false;
-        if(leftS <= rightD)
-            return false;
+void initjoueur(joueur &s)
+{
+    s.x=10;
+    s.y=SCREEN_HEIGHT/2;
+    s.w=15;
+    s.h=59;
+    s.lectureFront.x=0;
+    s.lectureFront.y=0;
+    s.lectureFront.w=15;
+    s.lectureFront.h=59;
+    s.lecture = s.lectureFront;
+}
 
-        return true;
-    }
+void showReticule(Souris &r)
+{
+milieuX=r.x;
+milieuY=r.y;
+
+}
+
+void initCanard(Canard &c)
+{
+    c.x=rand()%SCREEN_WIDTH;
+    c.y=rand()%SCREEN_HEIGHT;
+    c.h=16;
+    c.w=16;
+    c.lectureFront.x=0;
+    c.lectureFront.y=0;
+    c.lectureFront.w=16;
+    c.lectureFront.h=16;
+    c.lecture = c.lectureFront;
+}
+
+
+bool collision(SDL_Surface* Screen, Canard D)
+{
+    int leftD;
+    int rightD;
+    int topD;
+    int bottomD;
+
+    int leftS = Screen.x;
+    int rightS = Screen.x + Screen.w;
+    int topS = Screen.y;
+    int bottomS = Screen.y + Screen.h;
+
+    leftD = D.x;
+    rightD = D.x + D.w;
+    topD = D.y;
+    bottomD = D.y + D.h;
+
+    if(bottomS >= topD)
+        return false;
+    if(topS <= bottomD)
+        return false;
+    if(rightS >= leftD)
+        return false;
+    if(leftS <= rightD)
+        return false;
+
+    return true;
+}
